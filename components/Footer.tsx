64dae7344a43acfba9a46b2c53412e92d85f01f2
Tewodrosyeteji <teddytejinesh@gmail.com>
2023-08-24 17:28:54 +0300

    display category

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-xs text-black text-center">
+          &copy;2023 StoreName ,All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
